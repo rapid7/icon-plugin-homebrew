5205ab6fda7f94701261f2078c4c4215ab930c77
iconplugin-r7 <IntegrationAlliance@rapid7.com>
2019-11-13 10:44:43 -0800

    Brew formula update for icon-plugin version v3.8.3

--- icon-plugin.rb
+++ icon-plugin.rb
@@ -2,16 +2,16 @@
 class IconPlugin < Formula
   desc "Development tooling for InsightConnect plugins"
   homepage "https://rapid7.com"
-  version "3.8.2"
+  version "3.8.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/rapid7/icon-plugin-homebrew/releases/download/v3.8.2/icon-plugin_3.8.2_Darwin_x86_64.tar.gz"
-    sha256 "5156e193d7f6ca837d686bc46e0b0329f76443927e28e3b99e53b09122369c51"
+    url "https://github.com/rapid7/icon-plugin-homebrew/releases/download/v3.8.3/icon-plugin_3.8.3_Darwin_x86_64.tar.gz"
+    sha256 "4e6065e958c1574e6cc4d23d3ce0fa877914f01e591a57dce153b82670696e7b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/rapid7/icon-plugin-homebrew/releases/download/v3.8.2/icon-plugin_3.8.2_Linux_x86_64.tar.gz"
-      sha256 "8d3e92da3346f28b66560acb39efec491e0611ca2bb7a264dafd5997a7d17b33"
+      url "https://github.com/rapid7/icon-plugin-homebrew/releases/download/v3.8.3/icon-plugin_3.8.3_Linux_x86_64.tar.gz"
+      sha256 "216d2e7d7679953029c187d37cf4fb643955a43bdac408f3acfce92dab5b818e"
     end
   end
 
